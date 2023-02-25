8863f4d4c400c0b10699f3854180c8c2ba608348
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-02-26 12:30:08 +1300

    Added code from tutorial, also added reset button

--- App.js
+++ App.js
@@ -1,14 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
-export default function App() {
+export default class App extends React.Component {
+  state = {
+    value: 0,
+    totalTaps: 0
+  }
+
+  increment = () => {
+    this.setState({
+      value: this.state.value + 1,
+      totalTaps: this.state.totalTaps + 1
+    })
+    console.log("Value: " + (this.state.value + 1))
+  }
+
+  decrement = () => {
+    this.setState({
+      value: this.state.value - 1,
+      totalTaps: this.state.totalTaps + 1
+    })
+    console.log("Value: " + (this.state.value - 1))
+  }
+
+  render() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <Text>This is a test!!</Text>
+      <Text style={{fontSize: 75, marginBottom: -20}} >{this.state.value}</Text>
+      <Text style={{fontSize: 20, padding: 20}} >{this.state.totalTaps}</Text>
       <StatusBar style="auto" />
+      <View style={{flexDirection: 'row'}}>
+        <Button title="Decrease" onPress={this.decrement} />
+        <Text>   </Text>
+        <Button title="Increase" onPress={this.increment} />
+        </View>
+      <View style={{flexDirection: 'row'}}>
+        <Button title="Reset" onPress={() => this.setState({value: 0, totalTaps: 0})} />
+        </View>
     </View>
   );
+  }
 }
 
 const styles = StyleSheet.create({
