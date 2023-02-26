0b188aee8da028f697e1a021022a931a36c5292f
Flynn Stevens <flynn-stevens@live.nmit.ac.nz>
2023-02-26 17:57:04 +1300

    Played around with toggles

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Switch } from 'react-native';
 
 export default class App extends React.Component {
+
   state = {
     value: 0,
     totalTaps: 0
@@ -31,12 +32,15 @@ export default class App extends React.Component {
       <Text style={{fontSize: 20, padding: 20}} >{this.state.totalTaps}</Text>
       <StatusBar style="auto" />
       <View style={{flexDirection: 'row'}}>
-        <Button title="Decrease" onPress={this.decrement} />
+        <Button title="Decrease" onPress={this.decrement} color="red" />
         <Text>   </Text>
-        <Button title="Increase" onPress={this.increment} />
+        <Button title="Increase" onPress={this.increment} color="green" backgroundColor="blue" />
         </View>
       <View style={{flexDirection: 'row'}}>
         <Button title="Reset" onPress={() => this.setState({value: 0, totalTaps: 0})} />
+        </View>
+      <View style={{flexDirection: 'row'}}>
+        <Switch title="Toggle" onValueChange={this.setState = "false"} value = "true" />
         </View>
     </View>
   );
@@ -46,7 +50,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'lightblue',
     alignItems: 'center',
     justifyContent: 'center',
   },
